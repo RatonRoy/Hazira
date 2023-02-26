427b02f07bd8b66f20270e2eb66708411fb7c1e5
RatonRoy <royraton111@gmail.com>
2023-02-27 04:42:59 +0600

    clear default components and create new component components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FirstPageComponent } from './first-page/first-page.component';
+import { SecondPageComponent } from './second-page/second-page.component';
+import { HeaderComponent } from './first-page/header/header.component';
+import { HeaderTopComponent } from './first-page/header/header-top/header-top.component';
+import { HeaderNavbarComponent } from './first-page/header/header-navbar/header-navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FirstPageComponent,
+    SecondPageComponent,
+    HeaderComponent,
+    HeaderTopComponent,
+    HeaderNavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
