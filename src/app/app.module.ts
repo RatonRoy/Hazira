8539808235e91f4ab525be919c459242a6298df9
RatonRoy <royraton111@gmail.com>
2023-02-28 03:24:21 +0600

    complete the calendar section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { SecondPageComponent } from './second-page/second-page.component';
 import { HeaderComponent } from './first-page/header/header.component';
 import { HeaderTopComponent } from './first-page/header/header-top/header-top.component';
 import { HeaderNavbarComponent } from './first-page/header/header-navbar/header-navbar.component';
+import { SidebarComponent } from './first-page/sidebar/sidebar.component';
+import { CalendarComponent } from './first-page/sidebar/calendar/calendar.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { HeaderNavbarComponent } from './first-page/header/header-navbar/header-
     SecondPageComponent,
     HeaderComponent,
     HeaderTopComponent,
-    HeaderNavbarComponent
+    HeaderNavbarComponent,
+    SidebarComponent,
+    CalendarComponent
   ],
   imports: [
     BrowserModule,
