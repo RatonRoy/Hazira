97083d13d2e6d85c5d0d709d8438765e1c92f6d4
RatonRoy <royraton111@gmail.com>
2023-02-28 01:30:14 +0600

    complete the header section

--- src/app/first-page/header/header-navbar/header-navbar.component.ts
+++ src/app/first-page/header/header-navbar/header-navbar.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+// import { faGridHorizontal } from '@fortawesome/free-solid';
+import { faBarChart } from '@fortawesome/free-regular-svg-icons';
+
 
 @Component({
   selector: 'app-header-navbar',
   templateUrl: './header-navbar.component.html',
-  styleUrls: ['./header-navbar.component.css']
+  styleUrls: ['./header-navbar.component.css'],
 })
 export class HeaderNavbarComponent {
-
+  /* <i class="fa-solid fa-grid-horizontal"></i> */
+  // faGridHorizontal = faGridHorizontal;
+  faBarChart = faBarChart;
 }
