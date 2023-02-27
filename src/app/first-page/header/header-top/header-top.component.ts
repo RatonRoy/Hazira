61427e30d88081cbd4b574b8b9a3fdabb00dfcf1
RatonRoy <royraton111@gmail.com>
2023-02-27 07:40:38 +0600

    complete header top section for the first page

--- src/app/first-page/header/header-top/header-top.component.ts
+++ src/app/first-page/header/header-top/header-top.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { faDiscourse } from '@fortawesome/free-brands-svg-icons';
+import { faGear, faAngleDown } from '@fortawesome/free-solid-svg-icons';
+// import {}
 
 @Component({
   selector: 'app-header-top',
   templateUrl: './header-top.component.html',
-  styleUrls: ['./header-top.component.css']
+  styleUrls: ['./header-top.component.css'],
 })
 export class HeaderTopComponent {
-
+  faDiscourse = faDiscourse;
+  faGear = faGear;
+  faAngleDown = faAngleDown;
 }
