7658ed2521c74dc8b647f7ac0484bffaae3aa023
RatonRoy <royraton111@gmail.com>
2023-02-28 14:16:21 +0600

    complete the second sidebar

--- src/app/second-page/sidebar-second/sidebar-second.component.ts
+++ src/app/second-page/sidebar-second/sidebar-second.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
+import { faClock } from '@fortawesome/free-regular-svg-icons';
+import { faPager } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar-second',
   templateUrl: './sidebar-second.component.html',
-  styleUrls: ['./sidebar-second.component.css']
+  styleUrls: ['./sidebar-second.component.css'],
 })
 export class SidebarSecondComponent {
-
+  faUser = faUser;
+  faClock = faClock;
+  faPager = faPager;
 }
